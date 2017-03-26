75e29e940f74b46f275ef7b763a81df20bd0861d
iamkiko <constantinides.ch@gmail.com>
2017-03-26 16:42:11 +0200

    Answers part 1
    To be continued

--- scripts.js
+++ scripts.js
@@ -1,32 +1,49 @@
 // FILL IN THE FUNCTIONS BELOW
 
 var sprintFunctions = {
-  largestEl: function(){  
-    // your code here
-  },
+  largestEl: function(arr){  
+    return Math.max.apply(null, arr);    
   
-  reversed: function(){  
-    // your code here
+  }
+  
+  reversed: function(reversedString){  
+    reversedString = "christos";
+    return reversedString.split("").reverse().join(""); //split splits in to characters, reverse reverses them and join puts them back together to form a word
   },
 
   loudSnakeCase: function(){  
-    // your code here
+    //TO BE COMPLETED
   },
 
-  compareArrays: function(){ 
-    // your code here (replace the return)
-    return "Finish compareArrays first!" 
+  compareArrays: function(array1, array2){ 
+    return array1.toString() === array2.toString();
   },
 
-  fizzBuzz: function(){  
-    // your code here
+  fizzBuzz: function(num){  
+    var fizzArray = [];
+    for (i = 0; i <= num; i++) {
+      if (i % 3 === 0 && i % 5 === 0){
+          fizzArray.push("FIZZBUZZ");
+      } else if (i % 3 === 0) {
+        fizzArray.push("FIZZ");
+      } else if (i % 5 === 0) {
+        fizzArray.push("BUZZ");
+      } else {
+        fizzArray.push(i);
+      }
+    }
+    return fizzArray;
   },
 
-  myMap: function(){  
-    // your code here
+  myMap: function(arr, fn){  
+    var newArr = [];
+    for(i = 0; i < arr.length; i++){
+      newArr.push(fn(arr[i]));
+    }
+    return newArr;
   },
 
   primes: function(){  
-    // your code here
+    // TO BE COMPLETED
   },
 }
